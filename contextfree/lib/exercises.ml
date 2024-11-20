0694348077de116d7ea8b42ed2a5546475640a11
Metwees <matteocambedda@gmail.com>
2024-11-20 14:59:34 +0100

    Merge

--- contextfree/lib/exercises.ml
+++ contextfree/lib/exercises.ml
@@ -16,15 +16,88 @@ let todo : grammar =
 
 
 (* #### Exercise 1, easy (zero_n_one_n) *)
-let zero_n_one_n : grammar = todo
+let zero_n_one_n : grammar = 
+  {
+    symbols = [ S ];
+    terminals = [ '0'; '1' ];
+    productions =
+      [
+        S --> "0S1";
+        S --> "";
+      ];
+    start = S;
+  }
+
 
 
 (* #### Exercise 2, easy (palindromes) *)
-let palindromes : grammar = todo
+let palindromes : grammar = 
+  {
+    symbols = [ S ];
+    terminals = [ '0'; '1' ];
+    productions =
+      [
+        S --> "0S0";
+        S --> "1S1";
+        S --> "";
+      ];
+    start = S;
+  }
 
 
-(* #### Exercise 3, medium (balanced_parentheses) *)
-let balanced_parentheses : grammar = todo
+(* #### Exercise 1, easy (zero_n_one_n) *)
+let zero_n_one_n : grammar = 
+  {
+    symbols = [ S ];
+    terminals = [ '0'; '1' ];
+    productions =
+      [
+        S --> "0S1";
+        S --> "";
+      ];
+    start = S;
+  }
+
+
+
+(* #### Exercise 2, easy (palindromes) *)
+let palindromes : grammar = 
+  {
+    symbols = [ S ];
+    terminals = [ '0'; '1' ];
+    productions =
+      [
+        S --> "0S0";
+        S --> "1S1";
+        S --> "0";
+        S --> "1";
+        S --> "";
+      ];
+    start = S;
+  }
+
+
+(* #### Exercise 3, medium (balanced_parentheses)*)
+let balanced_parentheses : grammar = 
+  {
+  symbols = [ S ];
+  terminals = [ '(';')'; '[';']'; '{';'}' ];
+  productions =
+    [
+      S --> "(S}"; (*0*)
+      S --> ")S{";(*1*)
+      S --> "[S]";(*2*)
+      S --> "(S]";(*3*)
+      S --> "{S[";(*4*)
+      S --> "}S)";(*5*)
+      S --> "(S)";(*6*)
+      S --> "{S}";(*7*)
+      S --> "]S)";(*8*)
+      S --> "[S(";
+      S --> "";(*10*)
+    ];
+  start = S;
+}
 
 
 (* #### Exercise 4, hard (same_amount)
@@ -32,4 +105,20 @@ let balanced_parentheses : grammar = todo
    Hint: model the language of words where the number of 0's is
    one greater than the number of 1's and viceversa, then combine them.
 *)
-let same_amount : grammar = todo
+let same_amount : grammar = 
+  {
+    symbols = [ S ];
+    terminals = [ '0'; '1' ];
+    productions =
+      [
+        S --> "0S1"; 
+        S --> "1S0";
+        S --> "ASB";
+        S --> "";
+        A --> "0S";
+        A --> "0AB";
+        B --> "1S";
+        B --> "1BA";
+      ];
+    start = S;
+  }
