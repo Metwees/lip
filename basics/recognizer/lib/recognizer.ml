3fd1fd70f7e6f486fd3400ed9ddba22ecf63e7ba
Matteo Cambedda <matteocambedda@gmail.com>
2025-03-02 19:36:10 +0100

    .

--- basics/recognizer/lib/recognizer.ml
+++ basics/recognizer/lib/recognizer.ml
@@ -24,17 +24,31 @@ let lang3 l = match l with
 | _ -> false
 ;;
 
-let rec lang4_aux l = match l with 
-[] -> false
-| h::t -> if h=='1' then true else lang4_aux t;;
+let lang4_aux q e = match q with
+  0 when e='0' -> 0
+| 0 when e='1' -> 1
+| 1 when e='0' -> 1
+| 1 when e='1' -> 2
+| 2 when e='0' -> 2
+| _ -> -1
+;;
 
-let rec lang4 l = match l with
-[] 
-| ['1']
-| ['0'] -> false
-| h::t -> if h=='1' then lang4 t else lang4_aux t;;
+let lang4 l = 
+  if (List.fold_left lang4_aux 0 l = 2) then true else false
+;;
 
-let lang5 _ = failwith ""
+let lang5_aux q e = match q with
+  0 when e='0' -> 1
+| 0 when e='1' -> 2
+| 1 when e='0' -> 0
+| 2 when e='1' -> 1
+| _ -> -1
+;;
+
+let lang5 l = 
+match List.fold_left lang5_aux 0 l with 
+0 -> true 
+| _ ->false;;
     
 let recognizers = [lang1;lang2;lang3;lang4;lang5]
                   
