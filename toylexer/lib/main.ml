72f15f32afe94b3eb92c5bbeb965f2832cc2b441
Matteo Cambedda <matteocambedda@gmail.com>
2025-03-05 20:11:21 +0100

    Toy Parser

--- toylexer/lib/main.ml
+++ toylexer/lib/main.ml
@@ -23,19 +23,21 @@ let string_of_tokenlist tl =
 let string_of_frequencies fl =
   List.fold_left (fun s (t,n) -> s ^ ((string_of_token t) ^ " -> " ^ string_of_int n ^ "\n")) "" fl
 
-
+(*prende un token e la lista e restituisce quante volte appare il token all'interno di quest'ultima*)
 let n_frequency x l = 
   List.fold_left(fun count y -> if(x=y) then count+1 else count) 0 l
 ;;
 
+(*Prende la lista e per ogni token calcola quante volte appare, eliminando i token già selezionati*)
 let rec pair_list l = match l with
 [] -> []
 | h::t -> (h,n_frequency h l) :: pair_list (List.filter (fun x ->not(h=x)) t)
 ;;
 
   (* frequency : int -> 'a list -> ('a * int) list *)
-(*Prende in input una lista di token e rest*)
+(*Prende in input il numero e una lista di token e restituisce *)
 let frequency n l = 
+(*classifica : ordina i token in base al loro numero e poi con List.take prende i primi n elementi della lista*)
 let classifica = List.sort (fun (_,count1) (_,count2) -> compare count2 count1) (pair_list l) in 
 List.take n classifica
 ;;
--- toyparser/lib/lexer.mll
+++ toyparser/lib/lexer.mll
@@ -4,12 +4,18 @@
 
 let white = [' ' '\t']+
 let num = ['0'-'9']|['1'-'9']['0'-'9']*
+let digits_hex = ['0'-'9' 'A'-'F' 'a'-'f']
+let hex = "0" ('x'|'X') digits_hex+
 
 rule read_token =
   parse
   | white { read_token lexbuf }  
   | "(" { LPAREN }
   | ")" { RPAREN }
+  | "*" { MULTIPLY }
+  | "/" { DIVIDE }
   | "+" { PLUS }
+  | "-" { MINUS }
+  | hex { CONST (Lexing.lexeme lexbuf) }
   | num { CONST (Lexing.lexeme lexbuf) }
   | eof { EOF }
